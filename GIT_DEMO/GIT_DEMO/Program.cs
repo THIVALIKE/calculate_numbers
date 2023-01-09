05d1d194c468875647c89cd31a41c3c798548cf4
THIVALIKE <thivajm007@gmail.com>
2023-01-09 15:49:31 +0530

    add two numbers completed

--- GIT_DEMO/GIT_DEMO/Program.cs
+++ GIT_DEMO/GIT_DEMO/Program.cs
@@ -10,14 +10,18 @@ namespace GIT_DEMO
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine(Addtwonumbers(10,40));
         }
         #region Add Two num
-
+        public static int Addtwonumbers(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
         #endregion
 
         #region Add Two product
-
+        
         #endregion
     }
 }
